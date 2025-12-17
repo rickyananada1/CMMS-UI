ef5c2f840fd352381806f9b26eb49543246edf5c
Rizky Pradana Audy Setya Putra <82051597+audydeejay@users.noreply.github.com>
2025-12-17 16:58:49 +0700

    update SR
    update SR

--- src/views/pages/work-order/service-request/pages/service-request/TableServiceReqDetail.js
+++ src/views/pages/work-order/service-request/pages/service-request/TableServiceReqDetail.js
@@ -1,104 +1,21 @@
 /* eslint-disable */
 /* prettier-ignore-start */
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { DetailCard } from 'src/components/elements/cards'
-import { CContainer, CCol, CRow, CFormTextarea } from '@coreui/react'
+import { CContainer } from '@coreui/react'
+import { useParams } from 'react-router-dom'
 import { Table } from 'src/components/elements/table'
-import { useGetWOTracking } from '../service-request/services/getServiceReq'
+import { useGetTableServiceWO } from '../service-request/services/getServiceReq'
 import detailServiceReqTable from '../service-request/hooks/detailServiceReqTable'
-import useTimeFormatter from 'src/hooks/useTimeFormatter'
-import CardHeader from 'src/components/elements/cards/CardHeader'
-import { FaPaperclip } from 'react-icons/fa'
-import AttachmentDrawer from 'src/components/elements/drawer/AttachmentDrawer'
-
-
-const columns = [
-  {
-    header: 'Work Order',
-    accessorKey: 'work_order_id',
-    size: 120,
-    cell: ({ row, getValue, column }) => (
-      <div
-        style={{
-          width: `${column.getSize()}px`,
-          cursor: 'pointer',
-          color: '#007bff',
-          textDecoration: 'underline'
-        }}
-        onClick={(e) => {
-          e.stopPropagation();
-          setSelectedRow(row.original);
-          // resetSelectedTaskEtc();
-        }}
-      >
-        {getValue() || '-'}
-      </div>
-    ),
-  },
-  {
-    header: 'Description',
-    accessorKey: 'description',
-    size: 450,
-    cell: ({ getValue, column }) => (
-      <div
-        style={{
-          width: `${column.getSize()}px`,
-          pointerEvents: 'none',
-        }}
-      >
-        {getValue() || '-'}
-      </div>
-    ),
-  },
-  {
-    header: 'Work Type',
-    accessorKey: 'work_type',
-    size: 110,
-    cell: ({ getValue, column }) => (
-      <div
-        style={{
-          width: `${column.getSize()}px`,
-          pointerEvents: 'none',
-        }}
-      >
-        {getValue() || '-'}
-      </div>
-    ),
-  },
-  {
-    header: 'Status',
-    accessorKey: 'status',
-    size: 110,
-    cell: ({ getValue, column }) => (
-      <div
-        style={{
-          width: `${column.getSize()}px`,
-          pointerEvents: 'none',
-        }}
-      >
-        {getValue() || '-'}
-      </div>
-    ),
-  },
-]
 
 const TableServiceReqDetail = ({ mode, setAction, setTabIndex, setVisible }) => {
   const {
-    data,
-    isLoading,
-    dataFile,
-    isDrawerOpen,
-    setDrawerOpen,
-    selectedFile,
-    setSelectedFile,
-    handleOpenDrawer,
     selectedRow,
     setSelectedRow,
-    tableRef,
-    handleSearch,
+    detailTableRef,
+    serviceRequestDetailData,
+    selectedServiceReq,
     searchDebounce,
-    downloadWOTracking,
-    // resetSelectedTaskEtc,
   } = detailServiceReqTable({
     mode,
     setAction,
@@ -106,38 +23,119 @@ const TableServiceReqDetail = ({ mode, setAction, setTabIndex, setVisible }) => 
     setVisible,
   })
 
-  const { formatDate, formatDuration } = useTimeFormatter()
+  const columns = [
+    {
+      header: 'Work Order',
+      accessorKey: 'work_order_id',
+      size: 120,
+      cell: ({ row, getValue, column }) => (
+        <div
+          style={{
+            width: `${column.getSize()}px`,
+            cursor: getValue() ? 'pointer' : 'default',
+            color: getValue() ? '#007bff' : '#000',
+            textDecoration: getValue() ? 'underline' : 'none',
+          }}
+          onClick={(e) => {
+            e.stopPropagation()
+            setSelectedRow(row.original)
+          }}
+        >
+          {getValue() || '-'}
+        </div>
+      ),
+    },
+    {
+      header: 'Description',
+      accessorKey: 'description',
+      size: 450,
+      cell: ({ getValue, column }) => (
+        <div
+          style={{
+            width: `${column.getSize()}px`,
+            pointerEvents: 'none',
+          }}
+        >
+          {getValue() || '-'}
+        </div>
+      ),
+    },
+    {
+      header: 'Work Type',
+      accessorKey: 'work_type',
+      size: 110,
+      cell: ({ getValue, column }) => (
+        <div
+          style={{
+            width: `${column.getSize()}px`,
+            pointerEvents: 'none',
+          }}
+        >
+          {getValue() || '-'}
+        </div>
+      ),
+    },
+    {
+      header: 'Status',
+      accessorFn: (row) => row.status?.work_order_status_enum,
+      size: 110,
+      cell: ({ getValue, column }) => (
+        <div
+          style={{
+            width: `${column.getSize()}px`,
+            pointerEvents: 'none',
+          }}
+        >
+          {getValue() || '-'}
+        </div>
+      ),
+    },
+  ]
+
+  const [page, setPage] = useState(1)
+  const [limit, setLimit] = useState(10)
+
+  const { uuid } = useParams()
+  useEffect(() => {
+    if (!selectedServiceReq && uuid) {
+      getServiceReq.mutate({ id: uuid }, {
+        onSuccess: (data) => dispatch(serviceRequestActions.setServiceRequestDetailData(data))
+      })
+    }
+  }, [uuid])
+
   return (
-    <>
-      <DetailCard className='mt-[-50px] mb-[20px]'>
-        <CContainer fluid>
-          <div className="flex items-center justify-between -mx-2">
-            <p className="mt-2 text-xl font-bold">
-              Work Order Related to Service Request
-            </p>
-          </div>
-          <hr className="w-full h-[1px] mt-[8px] bg-neutral-stroke"></hr>
-          <Table
-            tableRef={tableRef}
-            columns={columns}
-            apiController={useGetWOTracking}
-            query={{
-              search: searchDebounce || undefined,
-            }}
-            selectableRowSelected={(row) =>
-              row.original?.work_order_id === selectedRow?.work_order_id
-            }
-            onRowClicked={(row) => {
-              setSelectedRow(row.original)
-              // resetSelectedTaskEtc()
-            }}
-            isAutoSelectFirstRow={false}
-            hasAutoNumber
-            isWithSearchField
-          />
-        </CContainer>
-      </DetailCard>
-    </>
+    <DetailCard className='mt-[-50px] mb-[20px]'>
+      <CContainer fluid>
+        <p className="mt-2 text-xl font-bold">
+          Work Order Related to Service Request
+        </p>
+        <hr className="w-full h-[1px] mt-[8px] bg-neutral-stroke" />
+        {console.log('SR ID (detaill):', selectedServiceReq?.uuid)
+        }
+        <Table
+          storeKey="service-request-detail"
+          data={serviceRequestDetailData}
+          tableRef={detailTableRef}
+          columns={columns}
+          apiController={useGetTableServiceWO}
+          getRowId={(row) => String(row.work_order_id)}
+          query={{
+            id: selectedServiceReq?.uuid,
+            page,
+            limit,
+            search: searchDebounce,
+          }}
+          selectableRowSelected={(row) =>
+            row.original?.work_order_id === selectedRow?.work_order_id
+          }
+          onRowClicked={(row) => setSelectedRow(row.original)}
+          isAutoSelectFirstRow={false}
+          hasAutoNumber
+          isWithSearchField
+        />
+      </CContainer>
+    </DetailCard>
   )
 }
 
