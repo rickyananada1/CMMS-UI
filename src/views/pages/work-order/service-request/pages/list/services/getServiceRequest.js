3f87b18840e3542339ffead3a4ef44ddc747e718
Rizky Pradana Audy Setya Putra <82051597+audydeejay@users.noreply.github.com>
2026-01-02 15:44:12 +0700

    delete consol

--- src/views/pages/work-order/service-request/pages/list/services/getServiceRequest.js
+++ src/views/pages/work-order/service-request/pages/list/services/getServiceRequest.js
@@ -4,7 +4,6 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'src/libs/axios'
 
 const getServiceRequest = async ({ params, signal }) => {
-  console.log(params, '[ppp');
   return await axios.get('/servicerequests', { params, signal })
 }
 
